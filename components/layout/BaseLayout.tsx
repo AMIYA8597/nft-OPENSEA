01a9df88140604299ec39dfecb1342473bcb3e70
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-04-11 03:38:44 +0530

    added until lec-3

---
+++ components/layout/BaseLayout.tsx
@@ -0,0 +1,22 @@
+import { FunctionComponent, ReactElement } from "react"
+import  Navbar  from "../navBar";
+interface childrenProps {
+    children : ReactElement | ReactElement[]
+}
+
+const BaseLayout: FunctionComponent<childrenProps> = (children) => {
+    return (
+        <div>  
+            
+            <Navbar />  
+            <div className="py-16 bg-grey-200 overflow-hidden">
+                <div className="max-w-7xl px-8 5m:px-8 lg:px-8" >
+                 {children}  
+                </div>
+            </div>
+
+        </div>  
+    )
+} 
+
+export default BaseLayout
