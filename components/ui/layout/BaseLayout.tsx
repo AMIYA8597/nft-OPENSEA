b429aeb553d86841d699fdb9f37805380f49a6ed
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-04-12 11:44:33 +0530

    fix BaseLayout Problem

--- components/ui/layout/BaseLayout.tsx
+++ components/ui/layout/BaseLayout.tsx
@@ -1,17 +1,19 @@
 import { FunctionComponent, ReactElement } from "react"
 import  Navbar  from "../navbar";
+
 interface childrenProps {
     children : ReactElement | ReactElement[]
 }
 
-const BaseLayout: FunctionComponent<childrenProps> = (children) => {
+const BaseLayout: FunctionComponent<childrenProps> = ({children}) => {
     return (
         <div>  
             
-            <Navbar />  
+            {/* <Navbar />   */}
+            
             <div className="py-16 bg-grey-200 overflow-hidden">
                 <div className="max-w-7xl px-8 5m:px-8 lg:px-8" >
-                 {children}  
+                 { children }
                 </div>
             </div>
 
