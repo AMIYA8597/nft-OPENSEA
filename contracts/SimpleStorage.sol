6b0d43ce259f1452d3411c221f583ee7a6075b47
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-04-13 15:37:11 +0530

    until lec-13 nut facing some error

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity <=0.8.20;
+
+contract SimpleStorage{
+    uint256 a;
+
+    function setter(uint256 _a) public {
+        a= _a+20;
+    }
+    // view --> normal --> pure 
+    function getter() public view returns(uint256){
+        // uint256 _a = a;
+        return a*20;
+    }
+}
---
+++ migrations/1_simpleStorage.js
@@ -0,0 +1,33 @@
+var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+
+module.exports = function (deployer) {
+    deployer.deploy(SimpleStorage);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var assert = require('assert');
+// contract('SimpleStorage', function(accounts) {
+//   it("should store the value 89", function() {
+//     return SimpleStorage.deployed().then(function(instance) {
+//       return instance.set("89", {from: accounts[0]});
+//     }).then(function(result) {
+//       assert.equal(result.logs[0].args._value, "89");
+//       return SimpleStorage.deployed();
+//     }).then(function(instance) {
+//       return instance.get.call();
+//     }).then(function(value) {
+//       assert.equal(value.toNumber(), 89);
+//     });
+//   });
