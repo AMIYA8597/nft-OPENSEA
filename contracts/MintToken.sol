2b89cdd374b870cd62393055c958259532938a1a
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-05-08 19:58:24 +0530

    testcase run successfully

--- contracts/MintToken.sol
+++ contracts/MintToken.sol
@@ -4,73 +4,241 @@ pragma solidity <=0.8.19;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NftMarket is ERC721URIStorage {
- using Strings for uint256;
+    using Strings for uint256;
 
-struct NftItem {
-    uint256 tokenId;
-    uint256 price;
-    address creator;
-    bool isListed;
-}
+    struct NftItem {
+        uint256 tokenId;
+        uint256 price;
+        address creator;
+        bool isListed;
+    }
 
-uint256 private _listedItemsCount;
-uint256 private _tokenIds;
+    uint256 private _listedItemsCount;
+    uint256 private _tokenIds;
+    mapping(string => bool) private _usedTokenURIs;
+    mapping(uint256 => NftItem) private _idNftItem;
 
-  uint256 private _listedItems;
-  uint256 private _tokenIdsCount;
-
-  mapping(string =>bool) private _usedTokenURIs;
-  mapping(uint256 => NftItem) private _idNftItem;
-
-  event NftItemCreated (
+    event NftItemCreated(
         uint256 tokenId,
         uint256 price,
         address creator,
         bool isListed
-  );
+    );
 
-  constructor() ERC721("CreatureNFT", "CNFT") {}
+    constructor() ERC721("CreatureNFT", "CNFT") {}
 
-  function getNftItem(uint256 tokenId) public view returns (NftItem memory){
-    return _idNftItem[tokenId];
-  }
+    function getNftItem(uint256 tokenId) public view returns (NftItem memory) {
+        return _idNftItem[tokenId];
+    }
 
-  function listedItemsCount() public view returns (uint256) {
-    return _listedItemsCount;
-  }
+    function listedItemsCount() public view returns (uint256) {
+        return _listedItemsCount;
+    }
 
-  function tokenURIExists (string memory tokenURI) public view returns (bool) {
-    return _usedTokenURIs[tokenURI];
+    function tokenURIExists(string memory tokenURI) public view returns (bool) {
+        return _usedTokenURIs[tokenURI];
+    }
 
-  }
+    function mintToken(string memory tokenURI, uint256 price)
+        public
+        payable
+        returns (uint256)
+    {
+        require(!_usedTokenURIs[tokenURI], "Token URI already exists");
+        _tokenIds++;
+        uint256 newTokenId = _tokenIds;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        _createNftItem(newTokenId, price);
+        _usedTokenURIs[tokenURI] = true;
+        _listedItemsCount++;
+        return newTokenId;
+    }
 
-  function mintToken(string memory tokenURI, uint256 price) public payable returns (uint256) {
-    
-    _tokenIds++;
-    _listedItems++;
+    function _createNftItem(uint256 tokenId, uint256 price) private {
+        _idNftItem[tokenId] = NftItem(tokenId, price, msg.sender, true);
+        emit NftItemCreated(tokenId, price, msg.sender, true);
+    }
 
-    uint256 newTokenId = _tokenIdsCount;
-
-    _safeMint(msg.sender, newTokenId); 
-    _setTokenURI(newTokenId, tokenURI);
-    _createNftItem(newTokenId, price);
-    _usedTokenURIs[tokenURI] = true;
-
-    return newTokenId;
-  }
-
-  function _createNftItem (uint256 tokenId, uint256 price) private {
-    require(price >0, "Price atleast 100 wei ");
-    _idNftItem[tokenId] = NftItem(tokenId, price, msg.sender,true);
-    emit NftItemCreated ( tokenId, price, msg.sender, true);
-
-  }
+    // New function to reset _listedItemsCount
+    function resetListedItemsCount() public {
+        _listedItemsCount = 0;
+    }
 }
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+// contract NftMarket is ERC721URIStorage {
+//     using Strings for uint256;
+
+//     struct NftItem {
+//         uint256 tokenId;
+//         uint256 price;
+//         address creator;
+//         bool isListed;
+//     }
+
+//     uint256 private _listedItemsCount;
+//     uint256 private _tokenIds;
+//     mapping(string => bool) private _usedTokenURIs;
+//     mapping(uint256 => NftItem) private _idNftItem;
+
+//     event NftItemCreated(
+//         uint256 tokenId,
+//         uint256 price,
+//         address creator,
+//         bool isListed
+//     );
+
+//     constructor() ERC721("CreatureNFT", "CNFT") {}
+
+//     function getNftItem(uint256 tokenId) public view returns (NftItem memory) {
+//         return _idNftItem[tokenId];
+//     }
+
+//     function listedItemsCount() public view returns (uint256) {
+//         return _listedItemsCount;
+//     }
+
+//     function tokenURIExists(string memory tokenURI) public view returns (bool) {
+//         return _usedTokenURIs[tokenURI];
+//     }
+
+//     function mintToken(string memory tokenURI, uint256 price)
+//         public
+//         payable
+//         returns (uint256)
+//     {
+//         require(!_usedTokenURIs[tokenURI], "Token URI already exists");
+//         _tokenIds++;
+//         uint256 newTokenId = _tokenIds;
+//         _safeMint(msg.sender, newTokenId);
+//         _setTokenURI(newTokenId, tokenURI);
+//         _createNftItem(newTokenId, price);
+//         _usedTokenURIs[tokenURI] = true;
+//         _listedItemsCount++;
+//         return newTokenId;
+//     }
+
+//     function _createNftItem(uint256 tokenId, uint256 price) private {
+//         _idNftItem[tokenId] = NftItem(tokenId, price, msg.sender, true);
+//         emit NftItemCreated(tokenId, price, msg.sender, true);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+// contract NftMarket is ERC721URIStorage {
+//  using Strings for uint256;
+
+// struct NftItem {
+//     uint256 tokenId;
+//     uint256 price;
+//     address creator;
+//     bool isListed;
+// }
+
+// uint256 private _listedItemsCount;
+// uint256 private _tokenIds;
+
+//   uint256 private _listedItems;
+//   uint256 private _tokenIdsCount;
+
+//   mapping(string =>bool) private _usedTokenURIs;
+//   mapping(uint256 => NftItem) private _idNftItem;
+
+//   event NftItemCreated (
+//         uint256 tokenId,
+//         uint256 price,
+//         address creator,
+//         bool isListed
+//   );
+
+//   constructor() ERC721("CreatureNFT", "CNFT") {}
+
+//   function getNftItem(uint256 tokenId) public view returns (NftItem memory){
+//     return _idNftItem[tokenId];
+//   }
+
+//   function listedItemsCount() public view returns (uint256) {
+//     return _listedItemsCount;
+//   }
+
+//   function tokenURIExists (string memory tokenURI) public view returns (bool) {
+//     return _usedTokenURIs[tokenURI];
+
+//   }
+
+//   function mintToken(string memory tokenURI, uint256 price) public payable returns (uint256) {
+    
+//     _tokenIds++;
+//     _listedItems++;
+
+//     uint256 newTokenId = _tokenIdsCount;
+
+//     _safeMint(msg.sender, newTokenId); 
+//     _setTokenURI(newTokenId, tokenURI);
+//     _createNftItem(newTokenId, price);
+//     _usedTokenURIs[tokenURI] = true;
+
+//     return newTokenId;
+//   }
+
+//   function _createNftItem (uint256 tokenId, uint256 price) private {
+//     require(price >0, "Price atleast 100 wei ");
+//     _idNftItem[tokenId] = NftItem(tokenId, price, msg.sender,true);
+//     emit NftItemCreated ( tokenId, price, msg.sender, true);
+
+//   }
+// }
 
 
 
