7b179d5ebddc9fa699527ef6d0745c0638e58843
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-06-24 17:37:27 +0530

    fix some issue

--- components/hooks/web3/setupHooks.ts
+++ components/hooks/web3/setupHooks.ts
@@ -1,20 +1,70 @@
 
-import { Web3Dependencies } from "@_types/hooks";
-import { hookFactory as createAccountHook, UseAccountHook } from "./useAccount";
-import { hookFactory as createNetworkHook, useNetworkHook } from "./useNetworks"
+// import { Web3Dependencies } from "@_types/hooks";
 
-export type Web3Hook = {
+import { Web3Dependencies } from "@_types/hooks"
+import { hookFactory as createAccountHook, UseAccountHook } from "./useAccount";
+import { hookFactory as createNetworkHook, UseNetworkHook } from "./useNetworks";
+import { hookFactory as createListedNftsHook, UseListedNftsHook } from "./useListedNfts";
+import { hookFactory as createOwnedNftsHook, UseOwnedNftsHook } from "./useOwnedNfts";
+
+export type Web3Hooks = {
   useAccount: UseAccountHook;
-  useNetwork: useNetworkHook
+  useNetwork: UseNetworkHook;
+  useListedNfts: UseListedNftsHook;
+  useOwnedNfts: UseOwnedNftsHook;
 }
 
-export type SetupHook = {
+export type SetupHooks = {
   (d: Web3Dependencies): Web3Hooks
 }
 
 export const setupHooks: SetupHooks = (deps) => {
   return {
     useAccount: createAccountHook(deps),
-    useNetwork: createNetworkHook(deps)
+    useNetwork: createNetworkHook(deps),
+    useListedNfts: createListedNftsHook(deps),
+    useOwnedNfts: createOwnedNftsHook(deps),
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Web3Dependencies } from "@_types/hooks";
+// import { hookFactory as createAccountHook, UseAccountHook } from "./useAccount";
+// import { hookFactory as createNetworkHook, useNetworkHook } from "./useNetworks"
+
+// export type Web3Hook = {
+//   useAccount: UseAccountHook;
+//   useNetwork: useNetworkHook
+// }
+
+// export type SetupHook = {
+//   (d: Web3Dependencies): Web3Hooks
+// }
+
+// export const setupHooks: SetupHooks = (deps) => {
+//   return {
+//     useAccount: createAccountHook(deps),
+//     useNetwork: createNetworkHook(deps)
+//   }
+// }
--- types/hooks.ts
+++ types/hooks.ts
@@ -1,11 +1,12 @@
 import { MetaMaskInpageProvider } from "@metamask/providers";
 import { Contract, providers } from "ethers"
 import { SWRResponse } from "swr";
+import { NftMarketContract } from "./nftMarketContract";
 
 
 export type Web3Dependencies = {
   provider: providers.Web3Provider;
-  contract: Contract;
+  contract: NftMarketContract;
   ethereum: MetaMaskInpageProvider;
   isLoading: boolean;
 }
@@ -38,37 +39,42 @@ export type CryptoSWRResponse<D = any, R = any> = SWRResponse<D> & R;
 
 
 
-// import { MetaMaskInpageProvider } from "@metamask/providers"
-// import { BrowserProvider, Contract } from "ethers"
-// import { SWRResponse } from "swr"
+
+
+
+
+
+
+
+
+
+
+
+
+// import { MetaMaskInpageProvider } from "@metamask/providers";
+// import { Contract, providers } from "ethers"
+// import { SWRResponse } from "swr";
 
 
 // export type Web3Dependencies = {
-//     ethereum : MetaMaskInpageProvider,
-//     provider : BrowserProvider|null,
-//     contract : Contract,
-//     isLoading : boolean
+//   provider: providers.Web3Provider;
+//   contract: Contract;
+//   ethereum: MetaMaskInpageProvider;
+//   isLoading: boolean;
 // }
 
-
-// export type CryptoHookFactory<D=any,R=any,P=any> = {
-//     (d:Partial<Web3Dependencies>) : CryptoHookHandler<D,R,P>
+// export type CryptoHookFactory<D = any, R = any, P = any> = {
+//   (d: Partial<Web3Dependencies>): CryptoHandlerHook<D, R, P>
 // }
-// export type CryptoHookHandler<D=any,R=any,P=any> = (params?: P) => CryptoHandlerResponse<D,any> & R
 
-// export type CryptoHandlerResponse<D=any,R=any> = SWRResponse<D> &R
+// export type CryptoHandlerHook<D = any, R = any, P = any> = (params?: P) => CryptoSWRResponse<D, R>
 
-
-
+// export type CryptoSWRResponse<D = any, R = any> = SWRResponse<D> & R;
 
 
-
-
-
-
-
-
-
+// // export type CryptoHookFactory<D = any, P = any> = {
+// //   (d: Partial<Web3Dependencies>): (params: P) => SWRResponse<D>
+// // }
 
 
 
@@ -89,15 +95,62 @@ export type CryptoSWRResponse<D = any, R = any> = SWRResponse<D> & R;
 // // import { BrowserProvider, Contract } from "ethers"
 // // import { SWRResponse } from "swr"
 
+
 // // export type Web3Dependencies = {
-// //     ethereum: MetaMaskInpageProvider,
-// //     provider: BrowserProvider,
-// //     contract: Contract
+// //     ethereum : MetaMaskInpageProvider,
+// //     provider : BrowserProvider|null,
+// //     contract : Contract,
+// //     isLoading : boolean
 // // }
 
-// // type CryptoHookFactory<D=any,R=any,p=any> = {
-// //     (d.Partial<web3Dependencies) : CryptoHandler<D, R, p>,
+
+// // export type CryptoHookFactory<D=any,R=any,P=any> = {
+// //     (d:Partial<Web3Dependencies>) : CryptoHookHandler<D,R,P>
 // // }
+// // export type CryptoHookHandler<D=any,R=any,P=any> = (params?: P) => CryptoHandlerResponse<D,any> & R
+
+// // export type CryptoHandlerResponse<D=any,R=any> = SWRResponse<D> &R
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // import { MetaMaskInpageProvider } from "@metamask/providers"
+// // // import { BrowserProvider, Contract } from "ethers"
+// // // import { SWRResponse } from "swr"
+
+// // // export type Web3Dependencies = {
+// // //     ethereum: MetaMaskInpageProvider,
+// // //     provider: BrowserProvider,
+// // //     contract: Contract
+// // // }
+
+// // // type CryptoHookFactory<D=any,R=any,p=any> = {
+// // //     (d.Partial<web3Dependencies) : CryptoHandler<D, R, p>,
+// // // }
 
 
 
