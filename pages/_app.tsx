a908b627eaf9cf1734453aa90b03e96ff462bb6f
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-06-26 13:36:30 +0530

    update pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,36 @@
+// import '../styles/globals.css'
+// import type { AppProps } from 'next/app'
+// import Navbar from '../components/ui/navbar';
+// import { Web3Provider } from '@/components/providers';
+// // import { Navbar } from '@/components';
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return ( 
+//     <>
+//     <Web3Provider>
+
+//     <Navbar />
+
+//   <Component {...pageProps} />
+
+//     </Web3Provider>
+//     </>
+//    )
+// }
+
+
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/ui/navbar';
 import { Web3Provider } from '@/components/providers';
-// import { Navbar } from '@/components';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return ( 
-    <>
+  return (
     <Web3Provider>
-
-    <Navbar />
-
-  <Component {...pageProps} />
-
+      <Navbar />
+      <Component {...pageProps} />
     </Web3Provider>
-    </>
-   )
+  );
 }
+
